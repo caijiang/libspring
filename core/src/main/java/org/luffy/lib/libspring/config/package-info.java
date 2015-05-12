24f483171014982188ffa7811cc893a3ff8c85b9
luffyj <luffy.ja@gmail.com>
2015-05-12 16:44:43 +0800

    doc changes

--- core/src/main/java/org/luffy/lib/libspring/config/package-info.java
+++ core/src/main/java/org/luffy/lib/libspring/config/package-info.java
@@ -6,8 +6,8 @@
 
 /**
  * <p>提供方便的配置文件对应各种功能模块的spring</p>
- * <p>要注意的是{@link libspring.loader.SimpleMVCLoader}会自动提供{@link libspring.config.JpaConfig}</p>
- * @see libspring.config.JpaConfig
+ * <p>要注意的是{@link org.luffy.lib.libspring.loader.SimpleMVCLoader}会自动提供{@link org.luffy.lib.libspring.config.JpaConfig}</p>
+ * @see org.luffy.lib.libspring.config.JpaConfig
  *
  **/
 package org.luffy.lib.libspring.config;
--- core/src/main/java/org/luffy/lib/libspring/loader/package-info.java
+++ core/src/main/java/org/luffy/lib/libspring/loader/package-info.java
@@ -6,7 +6,7 @@
 
 /**
  * <p>启动器</p>
- * <p>{@link libspring.loader.MutliDispatcherLoader}是一个多Dispatcher启动器，并不在常见场合中使用。</p>
+ * <p>{@link org.luffy.lib.libspring.loader.MutliDispatcherLoader}是一个多Dispatcher启动器，并不在常见场合中使用。</p>
  * <p>如需Spring Security请创建类继承与{@link org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer} 如下</p>
  * <pre class="code">
  *     public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
@@ -15,7 +15,7 @@
  *         }
  *     }
  * </pre>
- * <p>MVC的实现请创建类继承与{@link libspring.loader.SimpleMVCLoader}</p>
+ * <p>MVC的实现请创建类继承与{@link org.luffy.lib.libspring.loader.SimpleMVCLoader}</p>
  * <pre class="code">
  *     public class MyLoader extends SimpleMVCLoader{
  *     }
