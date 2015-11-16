8229dd9a7682c8a6d1171d3a7992d7be783b4ca3
HZCJ <luffy.ja@gmail.com>
2015-11-16 21:02:24 +0800

    update to 1.11

--- libspringtest/src/main/java/libspringtest/SpringWebTest.java
+++ libspringtest/src/main/java/libspringtest/SpringWebTest.java
@@ -1,7 +1,9 @@
 package libspringtest;
 
+import com.gargoylesoftware.htmlunit.WebClient;
 import org.junit.Before;
 import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.core.context.SecurityContext;
@@ -10,6 +12,10 @@ import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcConfigurer;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.inject.Inject;
@@ -23,15 +29,16 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
  * <p>Spring web测试基类</p> *
  * <p>典型测试如下</p>
  * <pre class="code">
- *     <code>@</code>ActiveProfiles("test")
- *     <code>@</code>ContextConfiguration(loader = Abc.class)
- *     实际上发现这样Load好像不太靠谱 老老实实一个一个Load吧……
- *     <code>@</code>RunWith(SpringJUnit4ClassRunner.class)
+ * <code>@</code>ActiveProfiles("test")
+ * <code>@</code>ContextConfiguration(loader = Abc.class)
+ * 实际上发现这样Load好像不太靠谱 老老实实一个一个Load吧……
+ * <code>@</code>RunWith(SpringJUnit4ClassRunner.class)
  * </pre>
  * <p>1.3 fixed bug</p>
+ *
+ * @author luffy luffy.ja at gmail.com
  * @see SpringContextLoader
  * @see ContextConfiguration
- * @author luffy luffy.ja at gmail.com
  */
 //@WebAppConfiguration
 //@ActiveProfiles("test")
@@ -40,51 +47,69 @@ public class SpringWebTest {
 
     /**
      * 自动注入应用程序上下文
-     * **/
+     **/
     @Inject
     protected WebApplicationContext context;
     /**
      * 自动注入servlet上下文
-     * **/
+     **/
     @Inject
     protected ServletContext servletContext;
     /**
      * 选配 只有在SecurityConfig起作用的情况下
-     * **/
+     **/
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired(required = false)
-    private FilterChainProxy springSecurityFilter;
+    protected FilterChainProxy springSecurityFilter;
 
+    @Autowired(required = false)
+    protected MockMvcConfigurer mockMvcConfigurer;
     /**
      * mock请求
-     * **/
+     **/
     @Autowired
     protected MockHttpServletRequest request;
 
     /**
      * mockMvc等待初始化
-     * **/
+     **/
     protected MockMvc mockMvc;
+    protected WebClient webClient;
+    protected WebDriver driver;
 
     @Before
-    public void creatMockMVC() {
+    public void createMockMVC() {
         MockitoAnnotations.initMocks(this);
-        if(springSecurityFilter!=null)
-            mockMvc = webAppContextSetup(context)
-                .addFilters(springSecurityFilter)
+        DefaultMockMvcBuilder builder = webAppContextSetup(context);
+        if (springSecurityFilter != null) {
+            builder = builder.addFilters(springSecurityFilter);
+        }
+
+        if (mockMvcConfigurer!=null){
+            builder = builder.apply(mockMvcConfigurer);
+        }
+        mockMvc = builder.build();
+
+        // 现在创建其他
+        webClient = MockMvcWebClientBuilder
+                .mockMvcSetup(mockMvc)
+                // DIY by interface.
                 .build();
-        else
-            mockMvc = webAppContextSetup(context)
-                    .build();
+
+        driver = MockMvcHtmlUnitDriverBuilder
+                .mockMvcSetup(mockMvc)
+                .build();
+
+
     }
 
     /**
      * 保存登陆过以后的信息
-     * **/
-    protected void saveAuthedSession(HttpSession session){
-        SecurityContext securityContext = (SecurityContext)   session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+     **/
+    protected void saveAuthedSession(HttpSession session) {
+        SecurityContext securityContext = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
 
-        if (securityContext==null)
+        if (securityContext == null)
             throw new IllegalStateException("尚未登录");
 
         request.setSession(session);
