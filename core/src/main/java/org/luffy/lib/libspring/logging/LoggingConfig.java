036b232eda34cd4d60bcf807078d293e28d13828
HZCJ <luffy.ja@gmail.com>
2016-01-07 16:15:25 +0800

    logging enhance.

--- core/src/main/java/org/luffy/lib/libspring/logging/LoggingConfig.java
+++ core/src/main/java/org/luffy/lib/libspring/logging/LoggingConfig.java
@@ -19,11 +19,18 @@ import org.apache.logging.log4j.core.config.AppenderRef;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * 通用可载入的日志配置辅助。
@@ -31,7 +38,7 @@ import java.util.Map;
  * <p>原则上，只要存在任何log4j.开头的属性(包括上下文属性和系统属性)都将该值应用到相关日志级别。</p>
  * <p>比如，设置了一个<code>log4j.org.luffy.lib</code>的属性，值为debug；则将生成新日志级别debug到org.luffy.lib</p>
  * <p>
- *     同样也可以应用于无配置文件，它会采用<code>log4j.root.level</code>作为默认日志级别，该功能在1.9以后生效。
+ * 同样也可以应用于无配置文件，它会采用<code>log4j.root.level</code>作为默认日志级别，该功能在1.9以后生效。
  * </p>
  * <strong>目前仅支持log4j2</strong>
  *
@@ -39,68 +46,67 @@ import java.util.Map;
  */
 @org.springframework.context.annotation.Configuration
 //@ComponentScan("com.huotu.test.bean")
-public class LoggingConfig {
+public class LoggingConfig implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Log log = LogFactory.getLog(LoggingConfig.class);
 
+    public static final String ROOT_LEVEL = "root.level";
+
     @Autowired(required = false)
     private ServletContext servletContext;
-
+    @Autowired
+    private ConfigurableEnvironment environment;
 
     @PostConstruct
     public void init() {
         try {
-            initLog4j2();
+            configLog4j(currentLoggingProperties());
         } catch (Throwable ignored) {
 
         }
-
-        log.info("LoggingConfig Info Level Log");
-        log.debug("LoggingConfig Debug Level Log");
-
-        log.warn("LoggingConfig Warn Level Log");
-        log.trace("LoggingConfig Trace Level Log");
     }
 
-    private void initLog4j2() {
-        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        final Configuration config = ctx.getConfiguration();
-//        Layout layout = PatternLayout.createLayout(PatternLayout.SIMPLE_CONVERSION_PATTERN, config, null,
-//                null,null, null);
-//        Appender appender = FileAppender.createAppender("target/test.log", "false", "false", "File", "true",
-//                "false", "false", "4000", layout, null, "false", null, config);
-//        appender.start();
-//        config.addAppender(appender);
-//        AppenderRef ref = AppenderRef.createAppenderRef("File", null, null);
-//        AppenderRef[] refs = new AppenderRef[] {ref};
+    /**
+     * <p>键为包名,值为键值</p>
+     * <p>优先级别为servlet参数,spring环境,系统环境</p>
+     * <p>必然包含root.level</p>
+     *
+     * @return 获取当前日志配置
+     */
+    public Properties currentLoggingProperties() {
 
-        //获取所有的appender
-        Map<String, Appender> appenderMap = config.getAppenders();
+        Properties properties = new Properties();
 
-        //如果没有配置 则默认输出级别更正为info 如果设置了 log4j.root.level 则依赖此项
-        if (appenderMap.size() == 1) {
-            String rootLevelName = System.getProperty("log4j.root.level", "info");
-            if (servletContext != null) {
-                String levelName = servletContext.getInitParameter("log4j.root.level");
-                if (levelName != null)
-                    rootLevelName = levelName;
-            }
-            ctx.getConfiguration().getRootLogger().setLevel(Level.toLevel(rootLevelName, Level.INFO));
-        }
+//        System.getProperties().stringPropertyNames()
+//                .stream()
+//                .filter(name -> name.startsWith("log4j."))
+//                .forEach(name -> {
+//                    String loggerName = name.substring(6);
+//                    properties.setProperty(loggerName, System.getProperty(name, "warn"));
+//                });
 
-        if (appenderMap.isEmpty())
-            return;
-        AppenderRef[] refs = appenderMap.keySet().stream().map(name -> AppenderRef.createAppenderRef(name, null, null)).toArray(AppenderRef[]::new);
-
-        System.getProperties().stringPropertyNames()
-                .stream()
-                .filter(name -> name.startsWith("log4j."))
+        environment.getSystemProperties().keySet().stream().filter(name -> name.startsWith("log4j."))
                 .forEach(name -> {
                     String loggerName = name.substring(6);
-                    LoggerConfig loggerConfig = LoggerConfig.createLogger("false", Level.toLevel(System.getProperty(name, "warn")), loggerName, "true", refs, null, config, null);
-                    config.addLogger(loggerName, loggerConfig);
-                    appenderMap.values().forEach(appender -> loggerConfig.addAppender(appender, null, null));
+                    properties.setProperty(loggerName
+                            , environment.getSystemProperties().getOrDefault(name, "warn").toString());
                 });
+        properties.setProperty(ROOT_LEVEL, environment.getProperty("log4j.root.level", "info"));
+
+        environment.getPropertySources().forEach(propertySource -> {
+//            if (propertySource.containsProperty("log4j.root.level")) {
+//                properties.setProperty(ROOT_LEVEL, propertySource.getProperty("log4j.root.level").toString());
+//            }
+            if (propertySource instanceof EnumerablePropertySource) {
+                String[] names = ((EnumerablePropertySource) propertySource).getPropertyNames();
+                Arrays.stream(names).filter(name -> name.startsWith("log4j."))
+                        .forEach(name -> {
+                            String loggerName = name.substring(6);
+                            properties.setProperty(loggerName
+                                    , propertySource.getProperty(name).toString());
+                        });
+            }
+        });
 
         if (servletContext != null) {
             Enumeration<String> names = servletContext.getInitParameterNames();
@@ -108,18 +114,54 @@ public class LoggingConfig {
                 String name = names.nextElement();
                 if (name.startsWith("log4j.")) {
                     String loggerName = name.substring(6);
-                    LoggerConfig loggerConfig = LoggerConfig.createLogger("false", Level.toLevel(servletContext.getInitParameter(name)), loggerName, "true", refs, null, config, null);
-                    config.addLogger(loggerName, loggerConfig);
-                    appenderMap.values().forEach(appender -> loggerConfig.addAppender(appender, null, null));
+                    properties.setProperty(loggerName, servletContext.getInitParameter(name));
                 }
             }
+//            String levelName = servletContext.getInitParameter("log4j.root.level");
+//            if (levelName != null) {
+//                properties.setProperty(ROOT_LEVEL, levelName);
+//            }
         }
 
-//        LoggerConfig loggerConfig = LoggerConfig.createLogger("false", "info", "org.apache.logging.log4j",
-//                "true", refs, null, config, null);
-//        loggerConfig.addAppender(appender, null, null);
-//        config.addLogger("org.apache.logging.log4j", loggerConfig);
+        return properties;
+    }
+
+    public void configLog4j(Properties properties) {
+        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = ctx.getConfiguration();
+
+        Map<String, Appender> appenderMap = config.getAppenders();
+
+        //如果没有配置 则默认输出级别更正为info 如果设置了 log4j.root.level 则依赖此项
+        if (appenderMap.size() == 1) {
+            ctx.getConfiguration().getRootLogger().setLevel(Level.toLevel(properties.getProperty(ROOT_LEVEL)
+                    , Level.INFO));
+        }
+
+        if (appenderMap.isEmpty())
+            return;
+        AppenderRef[] refs = appenderMap.keySet().stream()
+                .map(name -> AppenderRef.createAppenderRef(name, null, null)).toArray(AppenderRef[]::new);
+
+        properties.stringPropertyNames().stream()
+                .filter(name -> !Objects.equals(name, ROOT_LEVEL))
+                .forEach(loggerName -> {
+                    LoggerConfig loggerConfig = LoggerConfig.createLogger("false"
+                            , Level.toLevel(properties.getProperty(loggerName)), loggerName, "true", refs, null
+                            , config, null);
+                    config.addLogger(loggerName, loggerConfig);
+                    appenderMap.values().forEach(appender -> loggerConfig.addAppender(appender, null, null));
+                });
 
         ctx.updateLoggers();
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        try {
+            configLog4j(currentLoggingProperties());
+        } catch (Throwable ignored) {
+
+        }
     }
 }
