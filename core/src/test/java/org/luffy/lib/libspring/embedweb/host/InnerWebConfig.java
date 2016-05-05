a82c552aa43a40c8dfe0fe451d7f04e62f018df8
HZCJ <luffy.ja@gmail.com>
2016-05-05 17:55:46 +0800

    gogo

--- core/src/test/java/org/luffy/lib/libspring/embedweb/host/InnerWebConfig.java
+++ core/src/test/java/org/luffy/lib/libspring/embedweb/host/InnerWebConfig.java
@@ -1,11 +1,21 @@
 package org.luffy.lib.libspring.embedweb.host;
 
 import org.luffy.lib.libspring.embedweb.EmbedWeb;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ViewResolver;
 
 /**
  * @author CJ
  */
 public class InnerWebConfig implements EmbedWeb {
+
+    @Bean
+    public SimpleController simpleController() {
+        return new SimpleController();
+    }
+
     @Override
     public String name() {
         return "inner";
@@ -19,5 +29,22 @@ public class InnerWebConfig implements EmbedWeb {
     @Override
     public String publicResourcePath() {
         return "/inner/public";
+    }
+
+    /**
+     * 很显然 作为EWP开发 这里返回结果的视图 自然是取自 自身资源的
+     */
+    @Controller
+    public static class SimpleController {
+
+        @RequestMapping
+        public String hello() {
+            // 其实应该是寻找.. /inner/private
+            // 认真的说
+            ViewResolver viewResolver;
+            // 但它输入参数太少了 应该继续寻找底层
+            return "hello";
+        }
+
     }
 }
