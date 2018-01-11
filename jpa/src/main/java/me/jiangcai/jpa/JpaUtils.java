8049ea5ffee8c44b8433b4da527371abe8b15eae
Cai Jiang <luffy.ja@gmail.com>
2018-01-11 23:02:31 +0800

    移除实际代码；保持向下兼容。

--- jpa/src/main/java/me/jiangcai/jpa/JpaUtils.java
+++ jpa/src/main/java/me/jiangcai/jpa/JpaUtils.java
@@ -1,14 +1,6 @@
 package me.jiangcai.jpa;
 
-import org.springframework.beans.BeanUtils;
-
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import java.beans.PropertyDescriptor;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 /**
  * @deprecated 挪到 {@link me.jiangcai.crud.utils.JpaUtils}
@@ -22,27 +14,7 @@ public class JpaUtils {
      * @return 获取这个实体类的id class
      */
     public static Class<? extends Serializable> idClassForEntity(Class<?> entityClass) {
-        IdClass idClass = entityClass.getAnnotation(IdClass.class);
-        if (idClass != null)
-            //noinspection unchecked
-            return idClass.value();
-        //noinspection unchecked
-        return (Class<? extends Serializable>) Stream.of(BeanUtils.getPropertyDescriptors(entityClass))
-                // to field
-                // NoSuchFieldException
-                .map(pd -> {
-                    try {
-                        return entityClass.getDeclaredField(pd.getName());
-                    } catch (NoSuchFieldException e) {
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .filter(field -> field.getAnnotation(Id.class) != null)
-                .map(Field::getType)
-                .findAny()
-                .orElseGet(() -> getIdFromMethod(entityClass));
-//                .filter(propertyDescriptor -> propertyDescriptor.attributeNames())
+        return me.jiangcai.crud.utils.JpaUtils.idClassForEntity(entityClass);
     }
 
     /**
@@ -50,53 +22,7 @@ public class JpaUtils {
      * @return 获取这个实体类的id 的名字
      */
     public static String idFieldNameForEntity(Class<?> entityClass) {
-        IdClass idClass = entityClass.getAnnotation(IdClass.class);
-        if (idClass != null)
-            //noinspection unchecked
-            return null;
-        //noinspection unchecked
-        return Stream.of(BeanUtils.getPropertyDescriptors(entityClass))
-                // to field
-                // NoSuchFieldException
-                .map(pd -> {
-                    try {
-                        return entityClass.getDeclaredField(pd.getName());
-                    } catch (NoSuchFieldException e) {
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .filter(field -> field.getAnnotation(Id.class) != null)
-                .map(Field::getName)
-                .findAny()
-                .orElseGet(() -> getFieldNameFromMethod(entityClass));
+        return me.jiangcai.crud.utils.JpaUtils.idFieldNameForEntity(entityClass);
     }
 
-    private static Class getIdFromMethod(Class<?> entityClass) {
-        return Stream.of(BeanUtils.getPropertyDescriptors(entityClass))
-                .filter(pd -> pd.getReadMethod() != null && pd.getReadMethod().getAnnotation(Id.class) != null)
-                .map(PropertyDescriptor::getPropertyType)
-                .findAny()
-                .orElseGet(() -> {
-                    if (entityClass.getSuperclass() == null || entityClass.getSuperclass() == Object.class)
-                        throw new IllegalArgumentException(entityClass + " has no @Id field.");
-                    @SuppressWarnings("UnnecessaryLocalVariable")
-                    Class target = idClassForEntity(entityClass.getSuperclass());
-                    return target;
-                });
-    }
-
-    private static String getFieldNameFromMethod(Class<?> entityClass) {
-        return Stream.of(BeanUtils.getPropertyDescriptors(entityClass))
-                .filter(pd -> pd.getReadMethod() != null && pd.getReadMethod().getAnnotation(Id.class) != null)
-                .map(PropertyDescriptor::getName)
-                .findAny()
-                .orElseGet(() -> {
-                    if (entityClass.getSuperclass() == null || entityClass.getSuperclass() == Object.class)
-                        throw new IllegalArgumentException(entityClass + " has no @Id field.");
-                    @SuppressWarnings("UnnecessaryLocalVariable")
-                    String fieldName = idFieldNameForEntity(entityClass.getSuperclass());
-                    return fieldName;
-                });
-    }
 }
